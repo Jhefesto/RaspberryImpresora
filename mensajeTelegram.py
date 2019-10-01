bdabce5a5781f7bd0d0838ddbcfe0fa7ed8ab59f
Jonathan Aguilar <jvaguilaralvarado@gmail.com>
2019-10-01 08:35:34 -0500

    newversion

--- mensajeTelegram.py
+++ mensajeTelegram.py
@@ -1,14 +1,14 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
 import re
-# 817752267:AAF3cb3Lyv92QVbxqpUcw2IffeCtjSN3XP4
+
 
 def yo(bot, update):
     chat = update.message.chat_id
     bot.send_message(chat_id=chat,text="mi id="+str(chat))
 
 def main():
-    updater = Updater('817752267:AAF3cb3Lyv92QVbxqpUcw2IffeCtjSN3XP4')
+    updater = Updater('TOKENtelegramm')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('yo',yo))
     updater.start_polling()
